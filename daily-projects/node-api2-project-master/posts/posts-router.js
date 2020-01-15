aa474f75278488f9bd3daa9743007f2b2836a571
dislersd <dislersd@gmail.com>
2020-01-14 20:44:39 -0800

    created get routes for posts and post comments

--- daily-projects/node-api2-project-master/posts/posts-router.js
+++ daily-projects/node-api2-project-master/posts/posts-router.js
@@ -11,4 +11,18 @@ router.get("/", (req, res) => {
     );
 });
 
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => res.status(200).json(post))
+    .catch(err => res.status(500).json({ err, message: "error getting post" }));
+});
+
+router.get("/:id/comments", (req, res) => {
+  Post.findPostComments(req.params.id)
+    .then(comments => res.status(200).json(comments))
+    .catch(err =>
+      res.status(500).json({ err, message: "error getting comments" })
+    );
+});
+
 module.exports = router;
