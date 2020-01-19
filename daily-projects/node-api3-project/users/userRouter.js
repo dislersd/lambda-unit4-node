ee850690aa49101d94b93e79a3a05b66632d56e8
dislersd <dislersd@gmail.com>
2020-01-18 20:18:22 -0800

    set up get and post routes for users

--- daily-projects/node-api3-project/users/userRouter.js
+++ daily-projects/node-api3-project/users/userRouter.js
@@ -1,32 +1,42 @@
-const express = require('express');
+const express = require("express");
+const User = require("./userDb");
 
 const router = express.Router();
+router.use(express.json());
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
+  const user = req.body;
+  User.insert(user)
+    .then(response => res.status(200).json(response))
+    .catch(err => res.status(500).json({ message: "error creating user" }));
+});
+
+router.post("/:id/posts", (req, res) => {
   // do your magic!
 });
 
-router.post('/:id/posts', (req, res) => {
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.get();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: "error getting users" });
+  }
+});
+
+router.get("/:id", (req, res) => {
   // do your magic!
 });
 
-router.get('/', (req, res) => {
+router.get("/:id/posts", (req, res) => {
   // do your magic!
 });
 
-router.get('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // do your magic!
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
-});
-
-router.delete('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // do your magic!
 });
 
