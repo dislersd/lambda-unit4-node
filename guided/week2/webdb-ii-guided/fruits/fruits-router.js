650716a90954de1ec6bc2ed7be701b292c12c14f
dislersd <dislersd@gmail.com>
2020-01-21 09:05:35 -0800

    basic knex setup

--- guided/week2/webdb-ii-guided/fruits/fruits-router.js
+++ guided/week2/webdb-ii-guided/fruits/fruits-router.js
@@ -1,20 +1,25 @@
 const express = require("express"); // 1. Bring in express
 const knex = require("knex"); // 2. Bring in knex
 
+const knexConfiguration = {
+  // client answers: which type of database? (sqlite, postgres, mysql, oracle)?
+  client: 'sqlite3', // the db driver
+  // the rest will depend on the type of database
+  // connection could be a string or an object
+  connection: {
+    filename: './data/produce.db3'
+  },
+  useNullAsDefault: true, // ONLY needed for SQLite
+}
+
 // db represents a connection to the database
 const db = knex(knexConfiguration);
-
-// const db = knex({
-//   client: 'sqlite3',
-//   connection: {
-//     filename: './data/produce.db3'
-//   },
-//   useNullAsDefault: true
-// });
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
+  // SELECT * FROM fruits
+  // db.select('*').from('fruits').then().catch();
   db("fruits")
     .then(fruits => {
       res.json(fruits);
@@ -25,6 +30,9 @@ router.get("/", (req, res) => {
 });
 
 router.get("/:id", (req, res) => {
+
+  // SELECT * FROM fruits WHERE id = 2
+
   const { id } = req.params;
 
   db("fruits")
@@ -41,7 +49,7 @@ router.get("/:id", (req, res) => {
 router.post("/", (req, res) => {
   const fruitData = req.body;
   db("fruits")
-    .insert(fruitData)
+    .insert(fruitData) // With SQLite - By default it returns an array with the last id
     .then(ids => {
       db("fruits")
         .where({ id: ids[0] })
