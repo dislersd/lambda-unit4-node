11fe6dbd8a061dec85c9fab87903b7e455e37be5
dislersd <dislersd@gmail.com>
2020-01-28 11:02:18 -0800

    added knex session store

--- guided/week3/webauth-ii-guided/api/server.js
+++ guided/week3/webauth-ii-guided/api/server.js
@@ -1,15 +1,38 @@
-const session = require("express-session");
-
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 
+const session = require("express-session");
+const KnexSessionStore = require("connect-session-knex")(session);
+
 const authRouter = require("../auth/auth-router.js");
 const usersRouter = require("../users/users-router.js");
 
+const dbConnection = require("../database/dbConfig");
+
 const server = express();
 
+const sessionConfig = {
+  name: "cookieMonster",
+  secret: process.env.SESSION_SECRET || "this is secret", // used for cookie encryption
+  cookie: {
+    maxAge: 1000 * 60 * 10, //10 mins in ms
+    secure: false, // set to true in production, only send cookies over HTTPS
+    httpOnly: true // JS cannot access cookies on the browser
+  },
+  resave: false,
+  saveUninitialized: true, // read about it for GDBR compiance
+  store: new KnexSessionStore({
+    knex: dbConnection,
+    tablename: "sessions",
+    sidfieldname: "sid",
+    createtable: true,
+    clearInterval: 60000
+  })
+};
+
 server.use(helmet());
+server.use(session(sessionConfig)); // turn on sessions
 server.use(express.json());
 server.use(cors());
 
