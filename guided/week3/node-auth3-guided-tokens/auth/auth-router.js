6cfb6c82f7d821906f6aabae9b89453ee3f6388e
dislersd <dislersd@gmail.com>
2020-01-29 09:39:04 -0800

    created token signature function

--- guided/week3/node-auth3-guided-tokens/auth/auth-router.js
+++ guided/week3/node-auth3-guided-tokens/auth/auth-router.js
@@ -26,9 +26,9 @@ router.post("/login", (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
-        res.status(200).json({
-          message: `Welcome ${user.username}!`
-        });
+        const token = signToken(user);
+
+        res.status(200).json({ token });
       } else {
         res.status(401).json({ message: "Invalid Credentials" });
       }
@@ -37,5 +37,20 @@ router.post("/login", (req, res) => {
       res.status(500).json(error);
     });
 });
+
+function signToken(user) {
+  const payload = {
+    userId: user.id,
+    username: user.username
+  };
+
+  const secret = "secret";
+
+  const options = {
+    expiresIn: "1d"
+  };
+
+  return jwt.sign(payload, secret, options);
+}
 
 module.exports = router;
